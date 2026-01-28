506deb4d0cf4bd7703973a253515c97c2e7e19c5
Vidya Sagar Reddy Desu <35026133+vidya381@users.noreply.github.com>
2026-01-27 23:12:41 -0600

    Convert from Spring Boot to React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // TODO: Add state for temperature, scales, dark mode, etc.
+
+  return (
+    <div className="app">
+      <h1>Temperature Converter</h1>
+      {/* TODO: Add converter form */}
+      {/* TODO: Add voice input/output buttons */}
+      {/* TODO: Add thermometer visualization */}
+      {/* TODO: Add dark mode toggle */}
+    </div>
+  )
+}
+
+export default App
